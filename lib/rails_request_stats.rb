b09b55ae9c64e3f4cdf015da7e934ef147c12e61
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2016-01-09 22:56:32 -0500

    Simplify the way we require files in the RailsRequestStats module

--- lib/rails_request_stats.rb
+++ lib/rails_request_stats.rb
@@ -1,10 +1,4 @@
-require 'rails_request_stats/version'
-require 'rails_request_stats/request_stats'
-require 'rails_request_stats/report'
-require 'rails_request_stats/notification_subscribers'
-require 'rails_request_stats/stats/database_query_stats'
-require 'rails_request_stats/stats/object_space_stats'
-require 'rails_request_stats/stats/runtime_stats'
+Dir.glob(File.dirname(__FILE__) + '/**/*.rb') { |file| require file }
 
 module RailsRequestStats
 end
